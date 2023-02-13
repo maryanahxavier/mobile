5e0f80487b3e906fd2b13e73df8921a6481b5b65
maryanahxavier <maryanahxavier9@gmail.com>
2023-02-13 12:39:07 -0300

    ola

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet,Text, Image} from 'react-native'
+import logo from "./assets/logo.png"
+import Toggle from "./Toggle"
 
 export default function App() {
+  const [flexDirection, setFlexDirection] = useState('row')
+  const [justifyContent, setJustifyContent] = useState('center')
+  const [alignItems, setAlignItems] = useState('center')
+  const layoutStyle = { flexDirection, justifyContent, alignItems }
+
+  const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
+  const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Toggle
+        label={'Primary axis (flexDirection)'}
+        value={flexDirection}
+        options={flexDirectionOptions}
+        onChange={(option) => {
+          setFlexDirection(option)
+        }}
+      />
+      <Toggle
+        label={`${primaryAxis} distribution (justifyContent)`}
+        value={justifyContent}
+        options={justifyContentOptions}
+        onChange={(option) => {
+          setJustifyContent(option)
+        }}
+      />
+      <Toggle
+        label={`${secondaryAxis} alignment (alignItems)`}
+        value={alignItems}
+        options={alignItemsOptions}
+        onChange={(option) => {
+          setAlignItems(option)
+        }}
+      />
+      <Text>oi linda</Text>
+      <Image source={logo} style={styles.img} />
+      <View style={[styles.layout, layoutStyle]}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
     </View>
-  );
+  )
 }
+
+const flexDirectionOptions = ['row', 'column']
+const justifyContentOptions = [
+  'flex-start',
+  'center',
+  'flex-end',
+  'space-around',
+  'space-between',
+]
+const alignItemsOptions = ['flex-start', 'center', 'flex-end', 'stretch']
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
+  },
+  box: {
+    padding: 25,
+    backgroundColor: '#3B6CD4',
+    margin: 5,
+  },
+  img:{
+    width:50,
+    height: 50
+  }
+})
