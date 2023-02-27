2952ff9e75e33e026b5651f9df9afb12500b0723
maryanahxavier <maryanahxavier9@gmail.com>
2023-02-27 12:16:33 -0300

    hi lorena

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
-import { Options } from 'react-native/Libraries/Utilities/codegenNativeCommands'
 
 interface IToggle{
   label: string
